e928ba15a31cf0f2332fedcc8ccdfc6e6340ecda
18381826414 <liyl778@163.com>
2023-01-12 15:11:31 +0800

    序列化算法

---
+++ rpc-simple/src/main/java/com/lyj/serialize/protostuff/ProtostuffSerializer.java
@@ -0,0 +1,41 @@
+package com.lyj.serialize.protostuff;
+
+import com.lyj.serialize.Serializer;
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+/**
+ * @program: My-Rpc
+ * @description:
+ * @author: lyj
+ * @create: 2023-01-11 00:56
+ **/
+public class ProtostuffSerializer implements Serializer {
+    /**
+     * Avoid re applying buffer space every time serialization
+     */
+    private static final LinkedBuffer BUFFER = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+
+    @Override
+    public byte[] serialize(Object obj) {
+        Class<?> clazz = obj.getClass();
+        Schema schema = RuntimeSchema.getSchema(clazz);
+        byte[] bytes;
+        try {
+            bytes = ProtostuffIOUtil.toByteArray(obj, schema, BUFFER);
+        } finally {
+            BUFFER.clear();
+        }
+        return bytes;
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        T obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        return obj;
+    }
+}
